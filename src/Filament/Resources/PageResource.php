fa1c37e3b5f692b8eb1bd4c198c31a2cc5c6d49a
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-02-25 21:52:56 +0000

    Clean up indentations and import.

--- src/Filament/Resources/PageResource.php
+++ src/Filament/Resources/PageResource.php
@@ -2,26 +2,16 @@
 
 namespace Jaymeh\FilamentPages\Filament\Resources;
 
-use Filament\Forms\Components\Checkbox;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
-use Filament\Forms\Form;
-use Filament\Forms\Get;
-use Filament\Forms\Set;
+use Filament\Forms\Components\{ Checkbox, Grid, Section, TextArea, TextInput, Toggle };
+use Filament\Forms\{ Form, Get, Set };
 use Filament\Resources\Resource;
 use Filament\Tables;
-use Filament\Tables\Columns\IconColumn;
-use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\{ IconColumn, TextColumn };
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use Jaymeh\FilamentDynamicBuilder\Forms\Components\PageBuilder;
-use Jaymeh\FilamentPages\Filament\Resources\PageResource\Pages\CreatePage;
-use Jaymeh\FilamentPages\Filament\Resources\PageResource\Pages\EditPage;
-use Jaymeh\FilamentPages\Filament\Resources\PageResource\Pages\ListPages;
+use Jaymeh\FilamentPages\Filament\Resources\PageResource\Pages\{ CreatePage, EditPage, ListPages };
 
 class PageResource extends Resource
 {
@@ -44,72 +34,74 @@ class PageResource extends Resource
                     '2xl' => 8,
                 ])
                     ->schema([
-                        Section::make('General')->schema([
-                            TextInput::make('title')
-                                ->live()
-                                ->required()
-                                ->rules('max:255')
-                                ->live(onBlur: true)
-                                ->afterStateUpdated(function (Get $get, Set $set, $state, ?Model $record) {
-                                    if (! $get('permalink') && ! $record->isPublished()) {
-                                        $set('permalink', Str::slug($state));
-                                    }
-                                }),
-                            TextArea::make('description')
-                                ->rules([])
-                                ->required(),
-                            PageBuilder::make('content')
-                                ->required()
-                                ->label('Content')
-                                ->collapsible()
-                                ->rules(['required'])
-                                ->columnSpanFull()
-                                ->blockNumbers(false),
-                        ])->columnSpan([
-                            'md' => 2,
-                            'lg' => 4,
-                            'xl' => 4,
-                            '2xl' => 6,
-                        ]),
-                        Section::make('Page Settings')->schema([
-                            Toggle::make('published_at')
-                                ->onColor('success')
-                                ->offColor('danger')
-                                ->live(onBlur: true)
-                                ->label(function (Get $get) {
-                                    return $get('published_at') ? 'Published' : 'Unpublished';
-                                })
-                                ->dehydrateStateUsing(function (?Model $record, ?string $state) {
-                                    if (! $state) {
-                                        return null;
-                                    }
+                        Section::make('General')
+                            ->schema([
+                                TextInput::make('title')
+                                    ->live()
+                                    ->required()
+                                    ->rules('max:255')
+                                    ->live(onBlur: true)
+                                    ->afterStateUpdated(function (Get $get, Set $set, $state, ?Model $record) {
+                                        if (! $get('permalink') && ! $record->isPublished()) {
+                                            $set('permalink', Str::slug($state));
+                                        }
+                                    }),
+                                TextArea::make('description')
+                                    ->rules([])
+                                    ->required(),
+                                PageBuilder::make('content')
+                                    ->required()
+                                    ->label('Content')
+                                    ->collapsible()
+                                    ->rules(['required'])
+                                    ->columnSpanFull()
+                                    ->blockNumbers(false),
+                            ])->columnSpan([
+                                'md' => 2,
+                                'lg' => 4,
+                                'xl' => 4,
+                                '2xl' => 6,
+                            ]),
+                        Section::make('Page Settings')
+                            ->schema([
+                                Toggle::make('published_at')
+                                    ->onColor('success')
+                                    ->offColor('danger')
+                                    ->live(onBlur: true)
+                                    ->label(function (Get $get) {
+                                        return $get('published_at') ? 'Published' : 'Unpublished';
+                                    })
+                                    ->dehydrateStateUsing(function (?Model $record, ?string $state) {
+                                        if (! $state) {
+                                            return null;
+                                        }
 
-                                    if ($record->isPublished() && $state) {
-                                        return $record->published_at;
-                                    }
+                                        if ($record->isPublished() && $state) {
+                                            return $record->published_at;
+                                        }
 
-                                    return now();
-                                }),
-                            Checkbox::make('is_homepage')
-                                ->live()
-                                ->afterStateUpdated(function (Get $get, Set $set, $state) {
-                                    if ($state) {
-                                        $set('permalink', null);
-                                    }
-                                }),
-                            TextInput::make('permalink')
-                                ->required(fn (Get $get) => $get('is_homepage') === false)
-                                ->rules(['nullable', 'required_if:is_homepage,0', 'max:255'])
-                                ->hidden(fn (Get $get): bool => $get('is_homepage'))
-                                ->helperText(
-                                    'The URL of the page.'
-                                ),
-                        ])->columnSpan([
-                            'md' => 1,
-                            'lg' => 4,
-                            'xl' => 2,
-                            '2xl' => 2,
-                        ]),
+                                        return now();
+                                    }),
+                                Checkbox::make('is_homepage')
+                                    ->live()
+                                    ->afterStateUpdated(function (Get $get, Set $set, $state) {
+                                        if ($state) {
+                                            $set('permalink', null);
+                                        }
+                                    }),
+                                TextInput::make('permalink')
+                                    ->required(fn (Get $get) => $get('is_homepage') === false)
+                                    ->rules(['nullable', 'required_if:is_homepage,0', 'max:255'])
+                                    ->hidden(fn (Get $get): bool => $get('is_homepage'))
+                                    ->helperText(
+                                        'The URL of the page.'
+                                    ),
+                            ])->columnSpan([
+                                'md' => 1,
+                                'lg' => 4,
+                                'xl' => 2,
+                                '2xl' => 2,
+                            ]),
                     ]),
             ]);
     }
